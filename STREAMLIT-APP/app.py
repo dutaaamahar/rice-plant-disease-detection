f7c8553c2bc9f70923f20f0c21f00bd0612f067e
Dwi Duta Mahardewantoro <notdwidutam@gmail.com>
2024-08-07 07:42:46 +0700

    Add module, function and method docstring

--- STREAMLIT-APP/app.py
+++ STREAMLIT-APP/app.py
@@ -1,18 +1,33 @@
+"""
+Aplikasi Web Identifikasi Penyakit Tanaman Padi
+
+Modul ini mengimplementasikan aplikasi web untuk mengidentifikasi penyakit pada
+tanaman padi menggunakan model Convolutional Neural Network (CNN). Aplikasi ini
+memungkinkan pengguna untuk mengunggah gambar penyakit tanaman padi, memproses gambar,
+dan memprediksi jenis penyakit.
+"""
+
 import os
 import numpy as np
-import streamlit as st
 import tensorflow as tf
 from PIL import Image
+import streamlit as st
 
-# Load the pre-trained model
+# Memuat model yang telah dilatih sebelumnya
 model_path = os.path.join(os.path.dirname(__file__), 'models', 'saved_model_256_9010.h5')
 model = tf.keras.models.load_model(model_path)
 
-# Mapping index to class labels
+# Mapping indeks ke label kelas
 class_labels = ['Bacterialblight', 'Blast', 'Brownspot', 'Tungro', 'Unknown']
 
-# Streamlit app
 def main():
+    """
+    Fungsi utama untuk menjalankan aplikasi web Streamlit.
+
+    Fungsi ini mengatur antarmuka Streamlit, termasuk judul, deskripsi, uploader file
+    untuk mengunggah gambar, dan tombol prediksi. Fungsi ini menangani pengunggahan gambar,
+    menampilkan gambar, pra-pemrosesan, dan prediksi.
+    """
     st.write("""
         # Identifikasi Penyakit pada Tanaman Padi menggunakan Metode Convolutional Neural Network
         *Web App* ini dibuat sebagai syarat dalam menyelesaikan Program Studi Strata Satu (S-1)
@@ -26,32 +41,32 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Display the uploaded image
+        # Menampilkan gambar yang diunggah
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image.", use_column_width=False)
 
-        # Button for prediction
         if st.button("Predict"):
-            # Preprocess the image
+            # Pra-pemrosesan gambar
             img = tf.keras.utils.load_img(uploaded_file, target_size=(150, 150))
             x = tf.keras.utils.img_to_array(img)
-            x /= 255
+            x /= 255.0
             x = np.expand_dims(x, axis=0)
             images = np.vstack([x])
 
-            # Make predictions
+            # Melakukan prediksi
             classes = model.predict(images, batch_size=10)
 
-            # Assuming model output shape is (1, 5) for the 5 classes
+            # Mengasumsikan bentuk output model adalah (1, 5) untuk 5 kelas
             predicted_label = class_labels[np.argmax(classes[0])]
             predicted_proba = classes[0][np.argmax(classes[0])]
 
             st.write("## Prediction:")
-            st.write(f"The image is classified as: {predicted_label} with confidence: {predicted_proba:.2f}")
+            st.write(f"The image is classified as: {predicted_label} with confidence: "
+                     f"{predicted_proba:.2f}")
 
             st.write("## Prediction Probabilities:")
             for label, proba in zip(class_labels, classes[0]):
                 st.write(f"{label}: {proba:.2f}")
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
